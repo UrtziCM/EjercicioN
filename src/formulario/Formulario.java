f7541fc83ecba2964fd77677e8da7efa3c49fab9
UrtziCM <urcrmedev@gmail.com>
2023-10-21 12:46:49 +0200

    MODIFY Simple formatting

--- src/formulario/Formulario.java
+++ src/formulario/Formulario.java
@@ -7,17 +7,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 public class Formulario extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-    	Parent root = FXMLLoader.load(this.getClass().getResource("/fxml/form.fxml"));
-    	Scene scene = new Scene( root);
-    	
-        stage.setTitle("Formulario");
-        stage.setScene(scene);
-        stage.show();
-    }
-    public static void main(String[] args) {
+	@Override
+	public void start(Stage stage) throws IOException {
+		Parent root = FXMLLoader.load(this.getClass().getResource("/fxml/form.fxml"));
+		Scene scene = new Scene(root);
+
+		stage.setTitle("Formulario");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
